2b8172265e912fd1eeb06deb9bd5d3a8e9286141
Qhaleed <reslykadiri@gmail.com>
2025-04-02 13:38:34 +0800

    compatibility

--- app/main.py
+++ app/main.py
@@ -9,7 +9,7 @@ app = FastAPI()
 # Configure CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],  # Your React frontend URL
+    allow_origins=["http://localhost:3000", "https://adzu-chat.onrender.com/"],  # Your React frontend URL
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
