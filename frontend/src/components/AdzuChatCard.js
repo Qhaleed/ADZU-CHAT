18501a9b8e2e94e40ab68ca5fb670a4e1da93dbc
Qhaleed <reslykadiri@gmail.com>
2025-04-12 16:05:46 +0800

    removed config.js

--- frontend/src/components/AdzuChatCard.js
+++ frontend/src/components/AdzuChatCard.js
@@ -1,6 +1,5 @@
 import { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
-import { config } from '../config';
 import { FaGithub } from 'react-icons/fa';
 import "./AdzuChatCard.css";
 
