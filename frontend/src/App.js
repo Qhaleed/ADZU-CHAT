3ba4cffd23fcfb465b0f03b80dd65f132b2c5caa
Qhaleed <reslykadiri@gmail.com>
2025-05-07 18:50:23 +0800

    darkmode updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,4 +1,5 @@
 import "./App.css";
+import "./global.css";
 import ChatPage from "./pages/ChatPage.js";
 import LandingPage from "./pages/Landing.js";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
@@ -7,22 +8,26 @@ import NotFound from "./pages/NotFound.js";
 import { inject } from '@vercel/analytics';
 import FAQs from "./pages/FAQs.js";
 import MaintenancePage from "./pages/MaintenancePage.js";
+import { ThemeProvider } from "./context/ThemeContext";
+import DarkModeToggle from "./components/DarkModeToggle";
 
 inject();
 function App() {
   return (
-    <div className="app-container">
-      <Router>
-        <Routes>
-          <Route path="/maintentance" element={<LandingPage />} />
-          <Route path="/chat" element={<ChatPage />} />
-          <Route path="/faqs" element={<FAQs />} />
-          <Route path="/" element={<MaintenancePage />} />
-          <Route path="*" element={<NotFound />} /> {/* 404 Page */}
-
-        </Routes>
-      </Router>
-    </div>
+    <ThemeProvider>
+      <div className="app-container">
+        <DarkModeToggle />
+        <Router>
+          <Routes>
+            <Route path="/maintenance" element={<LandingPage />} />
+            <Route path="/chat" element={<ChatPage />} />
+            <Route path="/faqs" element={<FAQs />} />
+            <Route path="/" element={<MaintenancePage />} />
+            <Route path="*" element={<NotFound />} /> {/* 404 Page */}
+          </Routes>
+        </Router>
+      </div>
+    </ThemeProvider>
   );
 }
 
